52199aaf05c84bc61d6a66fa4bdc4b1fb066b35e
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2024-09-21 23:10:34 +0800

    update global.js

--- src/global.js
+++ src/global.js
@@ -3,7 +3,21 @@ const globalState = {};
 module.exports.get = () => globalState;
 
 module.exports.set = (key, value) => {
-    globalState[key] = value;
+    if(typeof key != "string")
+        throw new Error("key is not string");
+
+    const keys = key.split('.');
+    let result = globalState;
+    for(let i=0; i<keys.length; i++) {
+        let currKey = keys[i];
+        if(i === keys.length - 1) {
+            result[currKey] = value;
+        } else {
+            if(result[currKey] === undefined || typeof result[currKey] !== "object")
+                result[currKey] = {};
+            result = result[currKey];
+        }
+    }
 };
 
 module.exports.find = (key, defaultValue = null) => {
@@ -12,10 +26,10 @@ module.exports.find = (key, defaultValue = null) => {
 
     const keys = key.split('.');
     let result = globalState;
-    for(const key of keys) {
-        if(result[key] === undefined)
+    for(const currKey of keys) {
+        if(result[currKey] === undefined)
             return defaultValue;
-        result = result[key];
+        result = result[currKey];
     }
     return result;
 };
